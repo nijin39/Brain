821784fbf309fb99b6bbc0cd64893988f3fe6ddf
Kim Jong IL <jngkim@amazon.com>
2022-01-28 11:54:46 +0900

    Login UI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 import "../styles/globals.css";
+import { AmplifyAuthenticator } from "@aws-amplify/ui-react";
+import { Amplify } from "aws-amplify";
+import awsExports from "../src/aws-exports";
+
+Amplify.configure({ ...awsExports, ssr: true });
 
 export default function MyApp({ Component, pageProps }: any) {
   // Use the layout defined at the page level, if available
   const getLayout = Component.getLayout || ((page: any) => page);
 
-  return getLayout(<Component {...pageProps} />);
+  return getLayout(
+    <AmplifyAuthenticator>
+      <Component {...pageProps} />
+    </AmplifyAuthenticator>
+  );
 }
